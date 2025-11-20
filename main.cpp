b04f25487da063c1750bf21d1b6b2d21b60b92bf
250143 <250143@newuu.uz>
2025-11-20 12:51:54 +0500

    did untill 10

--- main.cpp
+++ main.cpp
@@ -115,3 +115,136 @@ int main() {
 }*/
 
 //p6
+/*void printIfValid(int* p) {
+    if (*p == -1) {
+        cout<<"null";
+    }
+    else {
+        cout<<*p;
+    }
+}
+int main() {
+    int i = 0;
+    int* p = new int[i];
+    int* p1 = p;
+    bool l = true;
+    int d = 0;
+    while (l) {
+        string input;
+        cin >> input;
+        if (input == "n") *(p+i) = -1;
+        else *(p+i) = stoi(input);
+        cout << "You wanna continue?(y/n)";
+        string answer;
+        cin >> answer;
+        if (answer == "n") {
+            l = false;
+        }
+        else i++;
+    }
+    while (i!=-1) {
+        *p1=*(p+d);
+        printIfValid(p1);
+        d++;
+        i--;
+        if (i!=-1) cout<<" , ";
+    }
+    delate [] p;
+    return 0 ;
+}*/
+
+//p7
+/*int* findMax(int* arr, int size) {
+    int* MaxValue = arr;
+    for (int* p = arr; p < arr + size; p++) {
+        if (*MaxValue < *p) MaxValue = p;
+    }
+    return MaxValue;
+}
+int main() {
+    int size;
+    cin >> size;
+    int* arr = new int[size];
+    for (int i = 0; i < size; i++) {
+        cin >> *(arr + i);
+    }
+    int* max = findMax(arr, size);
+    cout << *max << endl;
+}*/
+
+//p8
+/*void reverseArr(int** arr, int size) {
+    int* p = *arr;
+    int left = 0, right = size - 1;
+    while (left < right) {
+        int temp = *(p + left);
+        *(p + left) = *(p + right);
+        *(p + right) = temp;
+        left++;
+        right--;
+    }
+}
+int main() {
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+    int*p = arr;
+    reverseArr(&p,size);
+    for (int i = 0; i < size; i++) {
+        cout << *(p+i)<<" ";
+    }
+    return 0;
+}*/
+
+//p9
+/*void avg(const double* arr, int size, double* result) {
+    for (int i = 0; i < size; i++) {
+        *result += *(arr + i);
+    }
+    *result /= size;
+}
+int main() {
+    int size;
+    cin >> size;
+    double* arr = new double[size];
+    double result=0;
+    for (int i = 0; i < size; i++) {
+        cin >> *(arr + i);
+    }
+    avg(arr, size, &result);
+    cout << result;
+    delete [] arr;
+}*/
+
+//p10
+void mergeSorted(const int* a, int n, const int* b, int m, int* out) {
+    for (int i = 0; i < n; i++) {
+        *(out + i) = *(a + i);
+    }
+        for (int i = n; i < m+n; i++) {
+            for (int j = 0; j < m; j++) {
+                *(out + i) = *(b + j);
+            }
+        }
+
+    }
+int main() {
+    int n;
+    cin >> n;
+    int * a = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> *(a + i);
+    }
+    int m ;
+    cin >> m;
+    int * b = new int[m];
+    for (int i = 0; i < m; i++) {
+        cin >> *(b + i);
+    }
+    int out = 0;
+    mergeSorted(a, n, b, m, &out);
+    cout << out ;
+}
